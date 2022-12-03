44200b21350259c44a66aff40ea825de0b555229
= <=>
2022-12-03 23:15:56 +0000

    Revert "- added budget - need to be changed with names etc."
    This reverts commit 365f60794380e4d90340d89fdfcf3d2ea35c02c5.
    
    # Conflicts:
    #	src/main/java/com/budgetmanager/controllers/HomeController.java
    #	src/main/java/com/budgetmanager/controllers/RegistrationController.java
    #	src/main/java/com/budgetmanager/entities/Budget.java
    #	src/main/java/com/budgetmanager/entities/User.java
    #	src/main/java/com/budgetmanager/repositories/BudgetRepository.java
    #	src/main/java/com/budgetmanager/repositories/UserRepository.java
    #	src/main/java/com/budgetmanager/services/BudgetService.java
    #	src/main/java/com/budgetmanager/services/RegistrationService.java
    #	src/main/java/com/budgetmanager/services/UserService.java

---
+++ src/main/java/com/budgetmanager/controllers/HomeController.java
@@ -0,0 +1,10 @@
+package com.budgetmanager.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+}
--- src/main/java/com/budgetmanager/services/UserService.java
+++ src/main/java/com/budgetmanager/services/UserService.java
@@ -2,13 +2,9 @@ package com.budgetmanager.services;
 
 import com.budgetmanager.DTOs.UserLoginDto;
 import com.budgetmanager.DTOs.UserLoginDtoMapper;
-import com.budgetmanager.entities.User;
 import com.budgetmanager.repositories.UserRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,22 +17,5 @@ public class UserService {
 
     public Optional<UserLoginDto> findByCredentialsLogin(String login) {
         return userRepository.findByLogin(login).map(UserLoginDtoMapper::map);
-    }
-
-    public Optional<User> findUserById(Long id) {
-        return userRepository.findUserById(id);
-    }
-
-    public Optional<User> getLoggedUser() {
-        String loggedUserLogin = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByLogin(loggedUserLogin);
-    }
-    public List<User> findAllUsers() {
-        return new ArrayList<>(userRepository.findAll());
-    }
-
-
-    public Long getLoggedUserId() {
-        return getLoggedUser().get().getId();
     }
 }
