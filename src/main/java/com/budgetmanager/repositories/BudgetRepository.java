aac84f101c45ec17162a202f4521fa6dfbaa0189
= <=>
2023-01-07 04:09:30 +0000

    - BudgetService class refactored

--- src/main/java/com/budgetmanager/repositories/BudgetRepository.java
+++ src/main/java/com/budgetmanager/repositories/BudgetRepository.java
@@ -5,7 +5,6 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
